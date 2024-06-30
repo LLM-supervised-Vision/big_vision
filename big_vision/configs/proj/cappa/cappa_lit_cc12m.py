275be8b8ef2fe9c0824001a6d2b87a567eba611f
Ziteng Wang <zw2526@nyu.edu>
2024-06-30 04:47:39 +0000

    cappa lit

--- big_vision/configs/proj/cappa/cappa_lit_cc12m.py
+++ big_vision/configs/proj/cappa/cappa_lit_cc12m.py
@@ -132,7 +132,7 @@ def get_config(arg=None):
   config.lr = 0.001
   config.wd = 0.0001
   config.schedule = schedule
-  config.wandb = False
+  config.wandb = True
 
   config.seed = 0
 
--- scripts/cappa.sh
+++ scripts/cappa.sh
@@ -5,6 +5,7 @@ export TFDS_DATA_DIR=gs://us-central2-storage/tensorflow_datasets
 cd ~/austin_big_vision
 python -m big_vision.trainers.proj.cappa.generative \
   --config big_vision/configs/proj/cappa/cappa_lit_cc12m.py
+  --workdir gs://us-central2-storage/tensorflow_datasets/lit_cc12m/cappa_lit_cc12m_`date '+%m-%d_%H%M'`
   # --config big_vision/configs/proj/cappa/cappa_replication.py:batch_size=16384,total_samples=9.0,eval_only=False \
   # --workdir gs://us-central2-storage/tensorflow_datasets/cappa_replication_`date '+%m-%d_%H%M'`
   # --config big_vision/configs/proj/cappa/pretrain.py
