c6c7e39b648d430a3545ff1d63e2be1c6d93e93d
Ziteng Wang <zw2526@nyu.edu>
2024-09-03 20:22:27 +0000

    added run_llm for paligemma

--- scripts/paligemma_demo.sh
+++ scripts/paligemma_demo.sh
@@ -3,5 +3,5 @@
 
 cd ~/austin_big_vision
 export PYTHONPATH=$PYTHONPATH:/home/austinwang/austin_big_vision
-python /home/austinwang/austin_big_vision/big_vision/trainers/proj/paligemma/run.py \
+python /home/austinwang/austin_big_vision/big_vision/trainers/proj/paligemma/run_llm.py \
     --ckpt /home/austinwang/gemma2b.npz
