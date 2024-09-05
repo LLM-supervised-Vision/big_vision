b407b351b3cf59d9353c9bc6a7c58f577b063097
Ziteng Wang <zw2526@nyu.edu>
2024-09-05 18:03:26 +0000

    paligemma configs: - added vit variant option

--- big_vision/configs/proj/paligemma/paligemma_playground.py
+++ big_vision/configs/proj/paligemma/paligemma_playground.py
@@ -84,7 +84,8 @@ def add_eval(c, res, *, text_len=64, prefix, mode, **kw):
 def get_config(arg=None):
   c = bvcc.parse_arg(
       arg, res=224,
-      mode='generative', freeze_vit=False, loss_fn='softmax',
+      mode='generative', loss_fn='softmax',
+      freeze_vit=False, img_variant='B/16', 
       freeze_llm=True, llm_ckpt="full", llm_pool='none', llm_lr_mult=0.1, llm_dropout=0.0, llm_clean_vocab = False, llm_projection=False,
       batch_size=8192, total_samples=3.0, dtype='float32',
       debug=False, 
@@ -123,7 +124,7 @@ def get_config(arg=None):
   c.model = {}
   c.model.temperature_init = 1/0.07 if c.loss_fn == 'softmax' else 10.0
   c.model.bias_init = None if c.loss_fn == 'softmax' else -10.0
-  c.model.img = dict(variant='B/16', pool_type='none', head_zeroinit=False, scan=True, dtype_mm=c.dtype)
+  c.model.img = dict(variant=c.img_variant, pool_type='none', head_zeroinit=False, scan=True, dtype_mm=c.dtype)
   c.model.llm = dict(
     scan=True, dtype=c.dtype, 
     dropout=c.llm_dropout, lyrs_frozen=-1, pool=c.llm_pool, projection=c.llm_projection,
