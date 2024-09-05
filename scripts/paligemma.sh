96c81324a7f63e3af8caa67d32fbe96a43853254
Ziteng Wang <zw2526@nyu.edu>
2024-09-05 18:06:52 +0000

    paligemma configs: - added vit variant option

--- scripts/paligemma.sh
+++ scripts/paligemma.sh
@@ -4,4 +4,4 @@ export TFDS_DATA_DIR=gs://us-central2-storage/tensorflow_datasets
 
 cd ~/austin_big_vision
 python -m big_vision.trainers.proj.paligemma.train \
-    --config big_vision/configs/proj/paligemma/paligemma_playground.py:res=224,mode='contrastive',freeze_vit=False,freeze_llm=False,llm_ckpt='partial_frozen:9',llm_pool='eos',llm_dropout=0.0,llm_projection=True,batch_size=256,dtype='bfloat16',total_samples=3.0,debug=True
+    --config big_vision/configs/proj/paligemma/paligemma_playground.py:res=224,mode='contrastive',loss_fn='sigmoid',freeze_vit=False,img_variant='So400m',freeze_llm=False,llm_ckpt='partial_frozen:9',llm_pool='eos',llm_dropout=0.0,llm_projection=True,batch_size=256,dtype='bfloat16',total_samples=3.0,debug=True
