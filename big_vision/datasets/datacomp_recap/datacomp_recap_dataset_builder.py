2a0867583175a42c72c21bdf08bb0101e64fd591
Ziteng Wang <zw2526@nyu.edu>
2024-09-24 05:37:48 +0000

    datacomp recap: - added datacom recap (small scale)

--- big_vision/datasets/datacomp_recap/datacomp_recap_dataset_builder.py
+++ big_vision/datasets/datacomp_recap/datacomp_recap_dataset_builder.py
@@ -12,48 +12,49 @@ of textual descriptions.
 """
 
 class Builder(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for datacomp_recap dataset."""
+    """DatasetBuilder for datacomp_recap dataset."""
 
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
+    VERSION = tfds.core.Version('1.0.0')
+    RELEASE_NOTES = {
+        '1.0.0': 'Initial release.',
+    }
 
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Dataset metadata."""
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description="DataComp-Recap-1B dataset",
-        features=tfds.features.FeaturesDict({
-            'image': tfds.features.Text(),  # URL of the image
-            'url': tfds.features.Text(),
-            're_caption': tfds.features.Text(),
-            'org_caption': tfds.features.Text(),
-            'original_width': tf.int64,
-            'original_height': tf.int64,
-        }),
-        supervised_keys=None,
-        homepage=_HOMEPAGE,
-    )
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Dataset metadata."""
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description="DataComp-Recap-1B dataset",
+            features=tfds.features.FeaturesDict({
+                'image': tfds.features.Text(),  # URL of the image
+                'url': tfds.features.Text(),
+                're_caption': tfds.features.Text(),
+                'org_caption': tfds.features.Text(),
+                'original_width': tf.int64,
+                'original_height': tf.int64,
+            }),
+            supervised_keys=None,
+            homepage=_HOMEPAGE,
+        )
 
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-      """Returns SplitGenerators."""
-      return {
-          'train': self._generate_examples('recap_datacomp_1k.tfrecord'),
-      }
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        path = dl_manager._download_dir / 'datacomp_recap_train.tfrecord'
+        return {
+            'train': self._generate_examples(path),
+        }
 
-  def _generate_examples(self, filepath):
-      """Yields examples."""
-      dataset = tf.data.TFRecordDataset(filepath)
-      for i, raw_record in enumerate(dataset):
-          example = tf.train.Example()
-          example.ParseFromString(raw_record.numpy())
-          feature = example.features.feature
-          yield i, {
-              'image': feature['image'].bytes_list.value[0].decode('utf-8'),
-              'url': feature['url'].bytes_list.value[0].decode('utf-8'),
-              're_caption': feature['re_caption'].bytes_list.value[0].decode('utf-8'),
-              'org_caption': feature['org_caption'].bytes_list.value[0].decode('utf-8'),
-              'original_width': feature['original_width'].int64_list.value[0],
-              'original_height': feature['original_height'].int64_list.value[0],
-          }
+    def _generate_examples(self, filepath):
+        """Yields examples."""
+        dataset = tf.data.TFRecordDataset(filepath)
+        for i, raw_record in enumerate(dataset):
+            example = tf.train.Example()
+            example.ParseFromString(raw_record.numpy())
+            feature = example.features.feature
+            yield i, {
+                'image': feature['image'].bytes_list.value[0],
+                'url': feature['url'].bytes_list.value[0].decode('utf-8'),
+                're_caption': feature['re_caption'].bytes_list.value[0].decode('utf-8'),
+                'org_caption': feature['org_caption'].bytes_list.value[0].decode('utf-8'),
+                'original_width': feature['width'].int64_list.value[0],
+                'original_height': feature['height'].int64_list.value[0],
+            }
