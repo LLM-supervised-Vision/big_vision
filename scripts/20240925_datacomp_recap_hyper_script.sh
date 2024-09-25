e1f15074231a089af5b9af99ead0d6c121c44313
Ziteng Wang <zw2526@nyu.edu>
2024-09-25 22:02:40 +0000

    datacomp recap: - small changes

--- scripts/20240925_datacomp_recap_hyper_script.sh
+++ scripts/20240925_datacomp_recap_hyper_script.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# Function to handle Ctrl+C signal
+function handle_ctrl_c {
+    echo "Ctrl+C detected in merge_segments.sh. Exiting gracefully."
+    # Kill all child processes
+    pkill -P $$
+    exit 1
+}
+
+# Trap Ctrl+C signal
+trap handle_ctrl_c SIGINT
+
 if [ -d "/home/austinwang/tensorflow_datasets" ]; then
     rm -r /home/austinwang/tensorflow_datasets
 fi
 
-config=100
-num_jobs=2
+config=10k
+num_jobs=5
 
 for i in $(seq 0 $((num_jobs-1)))
 do
     echo "Starting job $((i))"
-    python /home/austinwang/austin_big_vision/scripts/20240924_datacomp_recap_construction.py --config $config --job_id $i --num_jobs $num_jobs
-    # sleep 1
+    python /home/austinwang/austin_big_vision/scripts/20240924_datacomp_recap_construction.py --config $config --job_id $i --num_jobs $num_jobs --gcs_tfds True &
+    sleep 1
 done
