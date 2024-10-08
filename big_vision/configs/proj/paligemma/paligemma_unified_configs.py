fecc01cff68ec391ff63f080ee2c0026f5ae0be8
Ziteng Wang <zw2526@nyu.edu>
2024-10-08 21:42:51 +0000

    developing paligemma unified configs

--- big_vision/configs/proj/paligemma/paligemma_unified_configs.py
+++ big_vision/configs/proj/paligemma/paligemma_unified_configs.py
@@ -73,14 +73,20 @@ def add_eval(c, res, *, text_len=64, prefix, mode, **kw):
   else:
     raise ValueError(f"Unknown mode: {mode}")
 
+backbone_dict = {
+  'gemma_2b': 'gs://us-central2-storage/tensorflow_datasets/gemma2b.npz',
+  'clip': 'gs://us-central2-storage/tensorflow_datasets/vit-b-16_3b_pretraining/clip_bs16384_warm0.03_lr1e-3_wd1e-4_bf16_qknorm-F_b2-0.95_12lyr_07-25_1415',
+  'gemma_supervised': 'gs://us-central2-storage/tensorflow_datasets/mllm_ckpts/paligemma/gemma2b-partial_frozen99-0.01-gap_b16-F_contrastive_bs16k_s3b_lr1e-3_wd1e-4_bf16_09-01_0446',
+}
+
 def get_config(arg=None):
   c = bvcc.parse_arg(
-    arg, res=224,
-    objective='contrastive', loss_fn='softmax', training_mode='pretrain',
-    dataset_name='laion400m/images', caption_key='caption',
-    freeze_vit=False, img_variant='B/16', img_beit_init=False, img_qknorm=False,
-    freeze_llm=True, llm_variant='gemma_2b', llm_ckpt="full", llm_head='none', llm_lr_mult=0.1, llm_dropout=0.0, llm_clean_vocab=False, llm_projection=False, llm_text_len=64,
-    drop_path_rate=0.0, lr=1e-3, wd=1e-4, batch_size=16384, total_epochs=-1, total_samples=-1.0, dtype='float32',
+    arg,
+    objective='contrastive', loss_fn='softmax', lr=1e-3, wd=1e-4, batch_size=16384, total_epochs=-1, total_samples=-1.0,
+    training_mode='pretrain', res=224, dataset_name='laion400m/images', caption_key='caption', backbone='gemma_2b',
+    img_variant='B/16', img_trainable='scratch', img_beit_init=False, img_qknorm=False,
+    llm_variant='gemma_2b', llm_trainable='full', llm_head='none', llm_lr_mult=0.1, llm_dropout=0.0, llm_clean_vocab=False, llm_projection=False, llm_text_len=64, 
+    drop_path_rate=0.0, dtype='float32',
     debug=False,
 	)
   
@@ -114,17 +120,17 @@ def get_config(arg=None):
     head=c.llm_head, projection=c.llm_projection, proj_bias=False if c.loss_fn == 'softmax' else True, drop_path_rate=c.drop_path_rate,
   )
 
+  if c.datacomp_backbone == 'gemma_supervised':
+    ckpt_cfg_path = f'{backbone_dict[c.backbone]}/config.json'
+    ckpt_cfg = ml_collections.ConfigDict(json.load(tf.io.gfile.GFile(ckpt_cfg_path, 'r')))
 
   # Checkpoint Loading Section
   img_backbone, llm_backbone = None, None
-  backbone_dict = {
-    'gemma_2b': 'gs://us-central2-storage/tensorflow_datasets/gemma2b.npz',
-    'clip': 'gs://us-central2-storage/tensorflow_datasets/vit-b-16_3b_pretraining/clip_bs16384_warm0.03_lr1e-3_wd1e-4_bf16_qknorm-F_b2-0.95_12lyr_07-25_1415',
-    'gemma_supervised': 'gs://us-central2-storage/tensorflow_datasets/mllm_ckpts/paligemma/gemma2b-partial_frozen99-0.01-gap_b16-F_contrastive_bs16k_s3b_lr1e-3_wd1e-4_bf16_09-01_0446',
-  }
+  c.model_load = {}
 
-  c.model_init = {'img': img_backbone, 'llm': llm_backbone}
+  backbone = backbone_dict[c.backbone]
 
+  # messy...
 
   dont_load = []
   if c.model.llm['head'] == 'map': dont_load += ['MAPHead.*']
